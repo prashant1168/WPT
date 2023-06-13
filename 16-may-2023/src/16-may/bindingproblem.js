bf9b9842d0ffacd11bb1bc68ead9257089074cf9
prashant1168 <131468232+prashant1168@users.noreply.github.com>
2023-06-14 03:40:30 +0530

    wpt

---
+++ 16-may-2023/src/16-may/bindingproblem.js
@@ -0,0 +1,18 @@
+let obj = {
+    fname:"AAA",
+    show: function(){
+        console.log(this.fname)
+    }
+}
+
+obj.show() //IF I call the function usinf obj  then this=obj(IMPLICIT)
+
+
+let f1 = obj.show
+f1() //function is called without object so this= undefined
+
+f1=f1.bind(obj) // EXPLICITLY binding the "this" with obj 
+f1()
+
+
+
